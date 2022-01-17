3ec40fa20be42003f535fcc19ddaf8896a9498f6
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-17 15:23:41 -0500

    React init setup

---
+++ cryptotogo/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+export default function App() {
+  return (
+    <Container textAlign='center'>
+      <Header as={'h1'}>Hello World</Header>
+    </Container>
+  )
+};
---
+++ cryptotogo/src/endpoints.ts
@@ -0,0 +1,4 @@
+// coingecko base URL
+const coingeckoBaseUrl = process.env.COINGECKO_API_URL;
+
+export const coingeckoCoinsList = `${coingeckoBaseUrl}/coins/list`;
