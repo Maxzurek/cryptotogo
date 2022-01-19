bcdd6bb28a65e21168ebf453115351fcf11a12e6
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-19 18:57:51 -0500

    Update MainNavBar.tsx

--- cryptotogo/src/components/navigation/MainNavBar.tsx
+++ cryptotogo/src/components/navigation/MainNavBar.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { Grid, Menu, GridRow, GridColumn, Item, Header } from "semantic-ui-react";
+import { Grid, Menu, GridRow, GridColumn, Item, Header, Image } from "semantic-ui-react";
 import SearchBar from "../utilities/SearchBar";
 
 interface MainNavBarProps {
@@ -8,22 +8,19 @@ interface MainNavBarProps {
 
 export default function MainNavBar(props: MainNavBarProps) {
     return (
-        <Grid as={Menu} stackable pointing secondary size='large' icon='labeled' color="blue" verticalAlign="bottom">
+        <Grid as={Menu} stackable pointing secondary size='large' color="blue" verticalAlign="bottom">
             <GridRow style={{ padding: 0 }}>
                 <GridColumn width={2}>
                     <Item as={NavLink} to='/'>
-                        <Header>Home</Header>
+                        <Image src="/images/CryptoToGo_Logo.ico" size="small" />
                     </Item>
                 </GridColumn>
-                <GridColumn width={4}>
+                <GridColumn width={3}>
                 </GridColumn>
                 <GridColumn width={6}>
                     <SearchBar />
                 </GridColumn>
                 <GridColumn width={4}>
-                <Item as={NavLink} to='/coin'>
-                        <Header>TEST REMOVE (to /coin)</Header>
-                    </Item>
                 </GridColumn>
             </GridRow>
         </Grid>
