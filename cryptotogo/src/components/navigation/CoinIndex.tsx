c10a0e8b09ca36c70a7eccc06c9f988187263021
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-19 18:33:37 -0500

    Update CoinIndex.tsx

--- cryptotogo/src/components/navigation/CoinIndex.tsx
+++ cryptotogo/src/components/navigation/CoinIndex.tsx
@@ -1,9 +1,32 @@
-interface CoinIndexProps{
+import { useLocation } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import { CoinDTO } from "../../models/coin.models";
+
+interface StateType {
+    coinDTO: CoinDTO
+}
+
+interface CoinIndexProps {
 
 }
 
 export default function CoinIndex(props: CoinIndexProps) {
-    return(
-        <>Coin Index</>
+
+    const location = useLocation();
+    const state = location.state as StateType;
+    const { coinDTO } = state;
+
+    return (
+        <Container fluid>
+            <div>
+                <h1>Id: {coinDTO.id}</h1>
+            </div>
+            <div>
+                <h1>Name: {coinDTO.name} </h1>
+            </div>
+            <div>
+                <h1>Current Price: (USD) {coinDTO.current_price?.usd}$</h1>
+            </div>
+        </Container>
     )
 };
