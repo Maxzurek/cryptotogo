f17fefebb5e217e0ce88c2551658bf2b533e96a3
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-19 17:44:23 -0500

    Update endpoints.ts

--- cryptotogo/src/endpoints.ts
+++ cryptotogo/src/endpoints.ts
@@ -3,8 +3,12 @@ const coingeckoBaseUrl = process.env.REACT_APP_COINGECKO_API_URL;
 
 export const coingeckoCoinsList = `${coingeckoBaseUrl}/coins/list`;
 
- // coingecko trending
- export const coingeckoCoinsTrending = `${coingeckoBaseUrl}/search/trending`;
- 
-  // coingecko search
- export const coingeckoSearch = `${coingeckoBaseUrl}/search?query=`
+// coingecko trending
+export const coingeckoCoinsTrending = `${coingeckoBaseUrl}/search/trending`;
+
+// coingecko search
+export const coingeckoSearch = `${coingeckoBaseUrl}/search?query=`
+
+// coingecko get coin info by id
+export const coingeckoCoinDataByIdStart = `${coingeckoBaseUrl}/coins/`
+export const coingeckoCoinDataByIdEnd = `?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`
