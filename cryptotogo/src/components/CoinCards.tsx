dc4915df372a45c2a6a5cdf6fbfe36ba3c323b85
FHARAKATE <fatimazahra.harakate@gmail.com>
2022-01-19 16:08:04 -0500

    coinDTO data creation

---
+++ cryptotogo/src/components/CoinCards.tsx
@@ -0,0 +1,34 @@
+import { Grid } from "semantic-ui-react"
+import { CoinDTO } from "../models/coin.models"
+import CoinDetail from "./CoinDetail"
+
+
+export default function CoinCards(props: coinCardsProps) {
+
+    const renderGridColumn = () => {
+
+        return (
+            props.theCoingeckoDTO.map((theCoinData, index) =>
+                <Grid.Column key={index}>
+                    <CoinDetail
+                        key={index}
+                        theCoinDetailDTO={theCoinData}
+                    />
+                </Grid.Column>
+            )
+        )
+    }
+
+    return (
+        <>
+            <Grid columns={3} container doubling stackable>
+                {renderGridColumn()}
+            </Grid>
+        </>
+    )
+}
+
+interface coinCardsProps {
+    theCoingeckoDTO: CoinDTO[];
+
+}
---
+++ cryptotogo/src/models/coin.models.d.ts
@@ -0,0 +1,17 @@
+
+// Coingecko Trending
+export interface CoinDTO{
+    id: string;
+    coin_id: number;
+    name: string;
+    symbol: string;
+    market_cap_rank : number
+    thumb : string
+    small:string
+    large:string
+    slug:string
+    price_btc : number
+    score:number
+}
+
+
