be532786f5fa4ba5f0305828a6a8b2a0bd9b19cd
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-20 11:03:12 -0500

    Update CoinDetail.tsx

--- cryptotogo/src/components/utilities/CoinDetail.tsx
+++ cryptotogo/src/components/utilities/CoinDetail.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import { useNavigate } from "react-router-dom";
 import { Card, Container, Image } from "semantic-ui-react";
 import { CoinDTO } from "../../models/coin.models";
+import AppDataContext from "../contexts/AppDataContext";
 
 interface coinDetailProps {
     theCoinDetailDTO: CoinDTO;
@@ -11,12 +12,13 @@ interface coinDetailProps {
 
 export default function CoinDetail(props: coinDetailProps) {
 
+    const { selectedCurrency } = useContext(AppDataContext);
     const [isFlipped, setFlipped] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
         setFlipped(!isFlipped);
-    }, [props])
+    }, [props.theCoinDetailDTO])
 
     const onClickCoin = () => {
         const coinDTO: CoinDTO = props.theCoinDetailDTO;
@@ -31,7 +33,12 @@ export default function CoinDetail(props: coinDetailProps) {
                     <Container as={'a'} fluid onClick={onClickCoin}>
                         <Image src={props.theCoinDetailDTO?.small} />
                         <Card.Header as='h2'>{props.theCoinDetailDTO?.name}{` (${props.theCoinDetailDTO?.symbol})`}</Card.Header>
-                        <Card.Description as='h3'> {`Last Price (USD) : ${props.theCoinDetailDTO?.current_price?.usd} $`}</Card.Description>
+                        <Card.Description as='h3'>
+                            {`Last Price (${selectedCurrency.toUpperCase()}) : ${props.theCoinDetailDTO?.current_price ?
+                                    props.theCoinDetailDTO.current_price[selectedCurrency]
+                                    :
+                                    undefined} $`}
+                        </Card.Description>
                         <Card.Meta as='h2'>{`Market Cap : ${props.theCoinDetailDTO?.market_cap_rank}`} </Card.Meta>
                     </Container>
                 </Card.Content>
