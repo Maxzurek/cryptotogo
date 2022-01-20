13673e879ba9c593d57c6f5c20800f3d1324c803
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-20 11:10:54 -0500

    Update SelectCurrency.tsx

--- cryptotogo/src/components/utilities/SelectCurrency.tsx
+++ cryptotogo/src/components/utilities/SelectCurrency.tsx
@@ -16,13 +16,8 @@ export default function SelectCurrency(props: selectCurrencyProps) {
         { key: 'usd', value: 'usd', text: 'USD' },
         { key: 'CAD', value: 'cad', text: 'CAD' },
         { key: 'EUR', value: 'eur', text: 'EUR' },
+        { key: 'ETH', value: 'eth', text: 'ETH' },
         { key: 'BTC', value: 'btc', text: 'BTC' },
-        { key: 'BNB', value: 'bnb', text: 'BNB' },
-        { key: 'XRP', value: 'xrp', text: 'XRP' },
-        { key: 'BITS', value: 'bits', text: 'BITS' },
-        { key: 'ZAR', value: 'zar', text: 'ZAR' },
-        { key: 'TWD', value: 'twd', text: 'TWD' },
-
     ]
 
     const onChangeCurrency = (event: React.SyntheticEvent<HTMLElement, Event>, data: any) => {
