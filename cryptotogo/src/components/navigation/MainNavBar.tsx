ced004c7c9885720243290060ab7a046f1ad0a7f
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-19 19:44:44 -0500

    Update MainNavBar.tsx
    Code cleanup

--- cryptotogo/src/components/navigation/MainNavBar.tsx
+++ cryptotogo/src/components/navigation/MainNavBar.tsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom";
-import { Grid, Menu, GridRow, GridColumn, Item, Header, Image } from "semantic-ui-react";
+import { Grid, Menu, GridRow, GridColumn, Item, Image } from "semantic-ui-react";
 import SearchBar from "../utilities/SearchBar";
 
 interface MainNavBarProps {
+    size?: 'mini' | 'tiny' | 'small' | 'large' | 'huge' | 'massive'
+}
 
+MainNavBar.defaultProps = {
+    size: 'large'
 }
 
 export default function MainNavBar(props: MainNavBarProps) {
     return (
-        <Grid as={Menu} stackable pointing secondary size='large' color="blue" verticalAlign="bottom">
+        <Grid as={Menu} stackable pointing secondary size={props.size} color="blue" verticalAlign="bottom">
             <GridRow style={{ padding: 0 }}>
                 <GridColumn width={2}>
                     <Item as={NavLink} to='/'>
