e91d53f3b17971769fcabc5b504f5599a8aba952
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-29 14:48:17 -0500

    Update LandingPage.tsx
    Removed br tag +code cleanup

--- cryptotogo/src/components/navigation/LandingPage.tsx
+++ cryptotogo/src/components/navigation/LandingPage.tsx
@@ -1,21 +1,18 @@
 import { useContext } from "react";
-import { Container, Dimmer, Header, Loader, Segment } from "semantic-ui-react";
+import { Container, Dimmer, Header, Loader } from "semantic-ui-react";
 import AppDataContext from "../contexts/AppDataContext";
 import CoinCards from "../utilities/CoinCards";
 
 
 export default function LandingPage() {
-    
+
     //Get coingeck Trending data, isLoadingData value and error Messages from  AppDataContext
     const { coingeckTrending, isLoadingData, errorMessage } = useContext(AppDataContext);
 
     return (
         <>
-            <br />
-            <Header size="huge" color="blue" textAlign="center">Top 6 Best Trending Coins</Header>
-            <br />
+            <Header size="huge" color="blue" textAlign="center" style={{marginTop: 30, marginBottom: 30}}>Top 6 Best Trending Coins</Header>
             <Container>
-
                 <Dimmer active={isLoadingData}>
                     <Loader>Loading</Loader>
                 </Dimmer>
