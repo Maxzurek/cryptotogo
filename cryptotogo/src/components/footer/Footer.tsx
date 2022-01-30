86a4fb0b9d794e6dbca77bce19cf9ada6f8a9f89
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-29 21:10:59 -0500

    Update Footer.tsx
    Added a container with padding top to prevent footer from hiding page content

--- cryptotogo/src/components/footer/Footer.tsx
+++ cryptotogo/src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Header, Icon, Menu, MenuItem } from "semantic-ui-react";
+import { Container, Icon, Menu, MenuItem } from "semantic-ui-react";
 import { ThemeContextProps } from "../../themes/theme.models";
 import { withTheme } from "../themeContext/withTheme";
 import SelectTheme from "../utilities/SelectTheme";
@@ -9,15 +9,17 @@ interface FooterProps extends ThemeContextProps {
 
 function Footer(props: FooterProps) {
     return (
-        <Menu fixed="bottom" icon='labeled' size="tiny">
-            <MenuItem>
-            <Icon name="theme" color={props.theme.primaryColor}/>
-                Choose a theme
-            </MenuItem>
-            <MenuItem style={{marginLeft: -15, paddingTop: 15}}>
-                <SelectTheme />
-            </MenuItem>
-        </Menu>
+        <Container style={{paddingTop: 80}}>
+            <Menu fixed="bottom" icon='labeled' size="tiny" style={{ marginTop: -10 }}>
+                <MenuItem>
+                    <Icon name="theme" color={props.theme.primaryColor} />
+                    Choose a theme
+                </MenuItem>
+                <MenuItem style={{ marginLeft: -15, paddingTop: 15 }}>
+                    <SelectTheme />
+                </MenuItem>
+            </Menu>
+        </Container>
     )
 };
 
