a7e01cee897e2f4d13aad63f400d0d6736aca442
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-29 21:00:14 -0500

    Update CoinCardPage.tsx
    Now fully responsive

--- cryptotogo/src/components/coinIndex/CoinCardPage.tsx
+++ cryptotogo/src/components/coinIndex/CoinCardPage.tsx
@@ -1,5 +1,5 @@
-import { useContext, useEffect, useState } from "react";
-import { Container, Grid, GridColumn, GridRow, Header, Image } from "semantic-ui-react";
+import { useContext, useEffect } from "react";
+import { Grid, GridColumn, GridRow, Header, Image } from "semantic-ui-react";
 import { CoinDTO } from "../../models/coin.models";
 import { ThemeContextProps } from "../../themes/theme.models";
 import AppDataContext from "../appDataContext/AppDataContext";
@@ -27,30 +27,33 @@ function CoinCardPage(props: CoinCardPageProps) {
 
     const renderCard = () => {
         return (
-
-            <Container >
-                <Grid verticalAlign="middle" centered >
-                    <GridRow style={{ padding: 30 }}>
-                        <GridColumn width={2} >
-                            <Image src={props.theCoinDetailDTO?.large} size="large" />
-                        </GridColumn>
-                        <GridColumn textAlign="center" width={4} >
-                            <Header as="h1" color={props.theme.primaryColor}>
-                                {props.theCoinDetailDTO?.name}{` (${props.theCoinDetailDTO?.symbol})`}
-                            </Header>
-                            <Header as="h4" color={props.theme.secondaryColor}>
-                                {`Latest Price (${selectedCurrency.toUpperCase()}) : ${props.theCoinDetailDTO?.current_price ?
-                                    props.theCoinDetailDTO.current_price[selectedCurrency]
-                                    :
-                                    undefined} ${selectedCurrency === 'eth' || selectedCurrency === 'btc' ? '' : '$'}`}
-                            </Header>
-                            <Header as="h5" color={props.theme.secondaryColor}>
-                                Current Time: {date}
-                            </Header>
-                        </GridColumn>
+            <Grid verticalAlign="middle" centered style={{ marginTop: 60, marginBottom: 60 }}>
+                <GridColumn width={4} style={{ padding: 0 }}>
+                    <div className="ui">
+                        <Image src={props.theCoinDetailDTO?.large} className="ui centered medium" />
+                    </div>
+                </GridColumn>
+                <GridColumn textAlign="left" width={6}>
+                    <GridRow style={{ marginBottom: 20 }}>
+                        <Header as="h1" color={props.theme.primaryColor}>
+                            {props.theCoinDetailDTO?.name}{` (${props.theCoinDetailDTO?.symbol})`}
+                        </Header>
                     </GridRow>
-                </Grid>
-            </Container>
+                    <GridRow style={{ marginBottom: 20 }}>
+                        <Header as="h3" color={props.theme.secondaryColor}>
+                            {`Latest Price (${selectedCurrency.toUpperCase()}) : ${props.theCoinDetailDTO?.current_price ?
+                                props.theCoinDetailDTO.current_price[selectedCurrency]
+                                :
+                                undefined} ${selectedCurrency === 'eth' || selectedCurrency === 'btc' ? '' : '$'}`}
+                        </Header>
+                    </GridRow>
+                    <GridRow>
+                        <Header as="h4" color={props.theme.secondaryColor}>
+                            Current Time: {date}
+                        </Header>
+                    </GridRow>
+                </GridColumn>
+            </Grid>
         )
     }
 
