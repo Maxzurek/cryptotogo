166824d2db56f88496d6ac9d60a3545f99059627
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-29 22:42:13 -0500

    Update Chart.tsx
    Added margins

--- cryptotogo/src/components/coinIndex/Chart.tsx
+++ cryptotogo/src/components/coinIndex/Chart.tsx
@@ -10,8 +10,15 @@ export default function Chart(props: ChartProps) {
     return (
         <div style={{ width: "100%", height: 300 }}>
             <ResponsiveContainer>
-                <AreaChart data={props.priceAction}
-                   >
+                <AreaChart
+                    data={props.priceAction}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
                     <defs>
                         <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
                             <stop offset="40%" stopColor="#82ca9d" stopOpacity={0.5} />
@@ -19,7 +26,7 @@ export default function Chart(props: ChartProps) {
                         </linearGradient>
                     </defs>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name"/>
+                    <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
                     <Area type="monotone" dataKey="price" stroke="#8884d8" fillOpacity={1} fill="url(#colorPrice)" />
