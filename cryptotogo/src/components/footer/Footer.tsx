1ae7b134de131c52abcae358d548b66495cbd377
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-30 00:54:22 -0500

    Update SelectTheme component

--- cryptotogo/src/components/footer/Footer.tsx
+++ cryptotogo/src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Container, Icon, Menu, MenuItem } from "semantic-ui-react";
+import { Container, Menu, MenuItem } from "semantic-ui-react";
 import { ThemeContextProps } from "../../themes/theme.models";
 import { withTheme } from "../themeContext/withTheme";
 import SelectTheme from "../utilities/SelectTheme";
@@ -9,12 +9,12 @@ interface FooterProps extends ThemeContextProps {
 
 function Footer(props: FooterProps) {
     return (
-        <Container style={{ paddingTop: 80 }}>
-            <Menu fixed="bottom" icon='labeled' size="tiny" style={{ marginTop: -10, backgroundColor: props.theme.backgroundColor }} >
-                <MenuItem>
-                    <Icon name="theme" color={props.theme.primaryColor} />
-                    Choose a theme
-                </MenuItem>
+        <Container style={{ paddingTop: 50, position: 'relative' }} fluid>
+            <Menu
+                icon='labeled'
+                size="tiny"
+                fluid
+                style={{ marginTop: -10, backgroundColor: props.theme.backgroundColor, position: 'absolute' }} >
                 <MenuItem style={{ marginLeft: -15, paddingTop: 15 }}>
                     <SelectTheme />
                 </MenuItem>
--- cryptotogo/src/components/utilities/SelectTheme.tsx
+++ cryptotogo/src/components/utilities/SelectTheme.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Select } from "semantic-ui-react";
+import { Dropdown, DropdownItem, DropdownItemProps, DropdownMenu, Icon, Select } from "semantic-ui-react";
 import { ThemeContextProps } from "../../themes/theme.models";
 import { DreamerTheme, CryptoToGoTheme, SolarizedTheme } from "../../themes/themes";
 import { withTheme } from "../themeContext/withTheme";
@@ -10,21 +10,36 @@ interface SelectThemeProps extends ThemeContextProps {
 
 function SelectTheme(props: SelectThemeProps) {
 
-    const [value, setValue] = useState('');
+    const [themeName, setThemeName] = useState('');
 
     const themes = [
-        { key: '1', value: CryptoToGoTheme.name, text: CryptoToGoTheme.name },
-        { key: '2', value: DreamerTheme.name, text: DreamerTheme.name },
-        { key: '3', value: SolarizedTheme.name, text: SolarizedTheme.name },
+        {
+            key: '1',
+            value: CryptoToGoTheme.name,
+            text: CryptoToGoTheme.name,
+            label: { color: CryptoToGoTheme.primaryColor, empty: true, circular: true },
+        },
+        {
+            key: '2',
+            value: DreamerTheme.name,
+            text: DreamerTheme.name,
+            label: { color: DreamerTheme.primaryColor, empty: true, circular: true },
+        },
+        {
+            key: '3',
+            value: SolarizedTheme.name,
+            text: SolarizedTheme.name,
+            label: { color: SolarizedTheme.primaryColor, empty: true, circular: true },
+        },
     ]
 
     useEffect(() => {
-        setValue(CryptoToGoTheme.name);
+        setThemeName(CryptoToGoTheme.name);
     }, [])
 
-    const onChangeTheme = (event: React.SyntheticEvent<HTMLElement, Event>, data: any) => {
+    const onChangeTheme = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, data: DropdownItemProps) => {
 
-        setValue(data.value)
+        setThemeName(data.value as string)
 
         switch (data.value) {
             case CryptoToGoTheme.name:
@@ -43,7 +58,20 @@ function SelectTheme(props: SelectThemeProps) {
 
     return (
         <>
-            <Select fluid options={themes} onChange={onChangeTheme} value={value} />
+            <Dropdown
+                item
+                trigger={<><Icon name='theme' color={props.theme.primaryColor} />{`Themes (${themeName})`}</>}
+                icon={null}
+            >
+                <DropdownMenu>
+                    <Dropdown.Menu scrolling>
+                        {themes.map((option) => (
+                            <DropdownItem {...option} onClick={onChangeTheme}/>
+                        ))}
+                    </Dropdown.Menu>
+                </DropdownMenu>
+            </Dropdown>
+            {/* <Select fluid options={themes} onChange={onChangeTheme} value={value} /> */}
         </>
     )
 };
