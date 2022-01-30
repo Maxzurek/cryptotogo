b5735c5050e190651147201d40cacd94608c6afc
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-29 19:13:37 -0500

    Create themes.ts

---
+++ cryptotogo/src/themes/themes.ts
@@ -0,0 +1,16 @@
+import { CryptoToGoTheming } from "./theme.models";
+
+export const PrimaryTheme : CryptoToGoTheming = {
+    primaryColor: "yellow",
+    secondaryColor: "blue"
+}
+
+export const DreamerTheme : CryptoToGoTheming = {
+    primaryColor: "violet",
+    secondaryColor: "teal"
+}
+
+export const SolatisTheme : CryptoToGoTheming = {
+    primaryColor: "orange",
+    secondaryColor: "yellow"
+}
